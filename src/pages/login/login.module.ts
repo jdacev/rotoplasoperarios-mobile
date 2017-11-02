04f4f9b91371bbfa25f65cf7ae22aea2921c52f3
martinoki <martinoki@hotmail.com>
2017-11-02 16:56:35 -0300

    Borradas declaraciones de mas
    Borradas declaraciones de mas

--- src/pages/login/login.module.ts
+++ src/pages/login/login.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { LoginPage } from './login';
-import { PasswordRecoveryPage } from '../password-recovery/password-recovery';
 
 @NgModule({
   declarations: [
-    LoginPage,
-    PasswordRecoveryPage
+    LoginPage
   ],
   imports: [
     IonicPageModule.forChild(LoginPage),
