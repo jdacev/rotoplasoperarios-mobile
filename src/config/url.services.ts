0248a7fa13ee36b7a9257d3d564bdc89daa69535
Dacev, Jorge Javier <jdacev@grupoassa.com>
2018-06-21 07:43:14 -0300

    marca de DEV en la home, URL a QA versión 2.2.1

--- src/config/url.services.ts
+++ src/config/url.services.ts
@@ -1,2 +1,3 @@
-export const URL_SERVICIOS = "https://rotoplasprod.herokuapp.com/api"; /*Produccion*/
-// export const URL_SERVICIOS = "https://rotoplasoperarios.herokuapp.com/api"; /*Desarrollo*/
+//export const URL_SERVICIOS = "https://rotoplasprod.herokuapp.com/api"; /* Produccion */
+export const URL_SERVICIOS = "https://rotoplasoperarios.herokuapp.com/api"; /* Desarrollo */
+//export const URL_SERVICIOS = "https://rotoplasqa.herokuapp.com/api"; /* QA */
