fd1c72e7a9e90f897b4683f47e93b4b3bc749dce
Domingo Hernández Cuervo <dhernandez@grupoassa.com>
2018-07-09 09:27:05 -0500

    mejoras toma de fotos / fotos con metadata lat, lng

--- src/pages/nueva-rutina/nueva-rutina.ts
+++ src/pages/nueva-rutina/nueva-rutina.ts
@@ -10,6 +10,7 @@ import { NetworkService } from "../../services/network-service";
 import { Network } from '@ionic-native/network';
 import { URL_SERVICIOS } from "../../config/url.services";
 import { Slides } from 'ionic-angular';
+import { Geolocation } from '@ionic-native/geolocation';
 
 @IonicPage()
 @Component({
@@ -19,31 +20,35 @@ import { Slides } from 'ionic-angular';
 export class NuevaRutinaPage {
   @ViewChild(Slides) slides: Slides;
 
-  loading:boolean;
-  ptarName:string;
-  ptarDate:string;
-  formato:string;
-  determinante:number;
+  loading: boolean;
+  ptarName: string;
+  ptarDate: string;
+  formato: string;
+  determinante: number;
   activities = [];
   tipoRutinas = [];
-  tipoRutina:string;
-  observacion:string;
+  tipoRutina: string;
+  observacion: string;
   actividadActual = [];
+  planta:any;
+  lat:any;
+  lng:any;
 
   images = [];
   imagesFiltro = [];
 
   constructor(public navCtrl: NavController,
-              public navParams: NavParams,
-              private rutinasProv: RutinasProvider,
-              private authservice: AuthService,
-              private camera: Camera,
-              private file: File,
-              private alertCtrl: AlertController,
-              private dbService: DatabaseService,
-              private networkService: NetworkService,
-              private network: Network,
-              private transfer: FileTransfer) {
+    public navParams: NavParams,
+    private rutinasProv: RutinasProvider,
+    private authservice: AuthService,
+    private camera: Camera,
+    private file: File,
+    private alertCtrl: AlertController,
+    private dbService: DatabaseService,
+    private networkService: NetworkService,
+    private network: Network,
+    private geolocation: Geolocation,
+    private transfer: FileTransfer) {
 
     this.loading = false;
     this.ptarName = this.authservice.AuthToken.planta.name;
@@ -53,6 +58,7 @@ export class NuevaRutinaPage {
     this.activities = [];
     this.tipoRutina = null;
     this.observacion = "";
+    this.planta = this.authservice.AuthToken.planta;
     // this.getTipoRutinas();
     this.getTipoRutinasOffline();
   }
@@ -62,20 +68,32 @@ export class NuevaRutinaPage {
   }
 
   //Vuelvo a la pantalla anterior.
-  cancel(){
+  cancel() {
     this.navCtrl.pop();
   }
 
   //Método para capturar imágenes y guardarlas en un array.
-  capturar(){
+  capturar() {
+    this.geolocation.getCurrentPosition().then((resp) => {
+      this.lat = resp.coords.latitude;
+      this.lng = resp.coords.longitude;
+      if(this.authservice.validaUbicacion(this.lat, this.lng)) {
+        this.presentToast('Usted se encuentra a una distancia mayor a la establecida para tomar fotografías');
+        return;
+      }
+    }).catch((error) => {
+      console.log('Error getting location', error);
+    });
+
+
     let idx = this.slides.getActiveIndex();
     // console.log(this.activities[idx]);
-    if(this.activities[idx].foto1__c !== undefined && this.activities[idx].foto2__c !== undefined) {
+    if (this.activities[idx].foto1__c !== undefined && this.activities[idx].foto2__c !== undefined) {
       this.presentToast('Soló puede agregar dos imagénes por actividad');
       return;
     };
     const options: CameraOptions = {
-      quality: 50,
+      quality: 40,
       destinationType: this.camera.DestinationType.FILE_URI,
       sourceType: this.camera.PictureSourceType.CAMERA,
       encodingType: this.camera.EncodingType.JPEG,
@@ -84,23 +102,50 @@ export class NuevaRutinaPage {
     }
     this.camera.getPicture(options).then((imagePath) => {
       let fileName = imagePath.substring(imagePath.lastIndexOf('/') + 1, imagePath.length);
-      if(this.activities[idx].foto1__c === undefined) {
+
+      if (this.activities[idx].foto1__c === undefined) {
         this.activities[idx].foto1__c = fileName;
-      } else if(this.activities[idx].foto1__c !== undefined) {
+      } else if (this.activities[idx].foto1__c !== undefined) {
         this.activities[idx].foto2__c = fileName;
       }
 
+      let metadata = {
+        id: this.activities[idx].id,
+        idtiporutina__c: this.activities[idx].idtiporutina__c,
+        actividad: this.activities[idx].name.normalize('NFD').replace(/[\u0300-\u036f]/g, ""),
+        orden__c: this.activities[idx].orden__c,
+        rutina__c: this.activities[idx].rutina__c,
+        sfid: this.activities[idx].sfid,
+        tipo_de_respuesta__c: this.activities[idx].tipo_de_respuesta__c || '-',
+        turno__c: this.activities[idx].turno__c || '-',
+        valor: this.activities[idx].valor || '-',
+        observacion: this.activities[idx].observacion.normalize('NFD').replace(/[\u0300-\u036f]/g, "") || '-',
+        latitude: this.lat || '-',
+        longitude: this.lng || '-'
+      };
+
       this.images.push({
-        path:imagePath,
-        metada:this.activities[idx]
+        path: imagePath,
+        metada: metadata
       });
       this.imagesFiltro.push({
-        path:imagePath,
-        metada:this.activities[idx]
-      })
+        path: imagePath,
+        metada: metadata
+      });
+
+      localStorage.setItem('actividad-parcial-img',JSON.stringify(this.images));
+      localStorage.setItem('actividad-parcial', JSON.stringify(this.activities));
     }, (err) => {
-     // Handle error
+      // Handle error
     });
+  }
+
+  private clean(obj) {
+    for (var propName in obj) { 
+      if (obj[propName] === null || obj[propName] === undefined) {
+        delete obj[propName];
+      }
+    }
   }
 
   private presentToast(text) {
@@ -114,131 +159,131 @@ export class NuevaRutinaPage {
   /*Muevo el archivo de la carpeta donde se guarda la imagen capturada,
   a una carpeta que 'rutinas' que le creo.
   */
-  moverArchivo(images:any[], id){
-    var dataDirectory=this.file.dataDirectory;
+  moverArchivo(images: any[], id) {
+    var dataDirectory = this.file.dataDirectory;
     var origen = dataDirectory + 'rutinas/'
     var sourceDirectory = images[0].path.substring(0, images[0].path.lastIndexOf('/') + 1);
     var destino = dataDirectory + 'rutinas/' + id.toString() + '/';
-    
+
     // console.log("ID EN MOVER ARCHIVO: " + id);
     // console.log("ID EN MOVER ARCHIVO: " + JSON.stringify(id));
-    
+
 
     //Verifico si existe el directorio 'rutinas'.
-    this.validarDirectorio(dataDirectory, 'rutinas').then(response=>{
-      if(response){
+    this.validarDirectorio(dataDirectory, 'rutinas').then(response => {
+      if (response) {
         //Existe la carpeta
         this.crearCarpetasId(origen, sourceDirectory, destino, images, id);
       }
-      else{
+      else {
         //NO Existe la carpeta 'rutinas', entonces la creo
-        this.file.createDir(dataDirectory, 'rutinas', false).then(data=>{
+        this.file.createDir(dataDirectory, 'rutinas', false).then(data => {
           this.crearCarpetasId(origen, sourceDirectory, destino, images, id);
-        }, err =>{
+        }, err => {
 
         });
       }
-    }, error =>{
+    }, error => {
 
     })
 
   }
 
   //Funcion que crea una carpeta con el nombre del ID para guardar las imágenes.
-  crearCarpetasId(origen, sourceDirectory, destino, images, id){
+  crearCarpetasId(origen, sourceDirectory, destino, images, id) {
     //Creo la carpeta con el nombre {ID}
-    this.file.createDir(origen, id.toString(), false).then(data=>{
+    this.file.createDir(origen, id.toString(), false).then(data => {
 
       //Muevo todas las imágenes al nuevo directorio.
       for (let i = 0; i < images.length; i++) {
 
-          var fileName = images[i].path.substring(images[i].path.lastIndexOf('/') + 1, images[0].path.length);
+        var fileName = images[i].path.substring(images[i].path.lastIndexOf('/') + 1, images[0].path.length);
 
 
-          this.file.moveFile(sourceDirectory,fileName,destino,fileName)
+        this.file.moveFile(sourceDirectory, fileName, destino, fileName)
           .then(
-            file=>{
+            file => {
 
             }, error => {
 
-          })
+            })
       }
 
-    }, err =>{
+    }, err => {
 
     });
   }
 
   // Funcion que verifica si existe un subdirectorio
-  validarDirectorio(dataDirectory, subDirectorio){
-    return new Promise(resolve=>{
+  validarDirectorio(dataDirectory, subDirectorio) {
+    return new Promise(resolve => {
       this.file.checkDir(dataDirectory, subDirectorio)
-                .then(_ => {
-                  resolve(true);
-                })
-                .catch(err => {
-                  resolve(false);
-      });
+        .then(_ => {
+          resolve(true);
+        })
+        .catch(err => {
+          resolve(false);
+        });
     });
   }
 
   //Función que elimina una imagen capturada.
-  eliminarImagen(pos:number, imagen:any){
+  eliminarImagen(pos: number, imagen: any) {
     var directorio = imagen.path.substring(0, imagen.path.lastIndexOf('/') + 1);
     var nombreArchivo = imagen.path.substring(imagen.path.lastIndexOf('/') + 1, imagen.path.length);
     this.file.removeFile(directorio, nombreArchivo);
     // this.images.splice(pos, 1)
-    
-    for(let i=0; i<this.activities.length; i++) {
-      if(this.images[i] === imagen) {
-        this.images.splice(i,1);
+
+    for (let i = 0; i < this.activities.length; i++) {
+      if (this.images[i] === imagen) {
+        this.images.splice(i, 1);
       }
-      if(this.activities[i].foto1__c === imagen.path.substring(imagen.path.lastIndexOf('/') + 1, imagen.path.length)) {
+      if (this.activities[i].foto1__c === imagen.path.substring(imagen.path.lastIndexOf('/') + 1, imagen.path.length)) {
         delete this.activities[i].foto1__c;
-      } else if(this.activities[i].foto2__c === imagen.path.substring(imagen.path.lastIndexOf('/') + 1, imagen.path.length)) {
+      } else if (this.activities[i].foto2__c === imagen.path.substring(imagen.path.lastIndexOf('/') + 1, imagen.path.length)) {
         delete this.activities[i].foto2__c;
       }
     }
     this.slideChanged();
   }
-/***********************************************************************/
+  /***********************************************************************/
 
-  getTipoRutinas(){
-    this.rutinasProv.getTipoRutinas().subscribe(data=>{
+  getTipoRutinas() {
+    this.rutinasProv.getTipoRutinas().subscribe(data => {
       this.tipoRutinas = data.data;
-    }, error=>{
+    }, error => {
 
     })
   }
 
-  getTipoRutinasOffline(){
-    this.dbService.getTipoRutinasOffline().then(response =>{
+  getTipoRutinasOffline() {
+    this.dbService.getTipoRutinasOffline().then(response => {
       this.tipoRutinas = response;
       // console.log("TIPO RUTINAS: " + JSON.stringify(response));
-      
-    }, error =>{
+
+    }, error => {
       console.log("ERROR getTipoRutinas" + JSON.stringify(error));
-      
+
     })
   }
 
-  getActividades(idTipoRutina:string, turno:string){
+  getActividades(idTipoRutina: string, turno: string) {
 
-    if(idTipoRutina && turno){
-        
-        
-        this.dbService.getPreguntasTipoRutinaOffline(idTipoRutina, turno).then(data =>{
-          // console.log("TIPOS RUTINA: " + JSON.stringify(data))
-          this.activities = data;
-            for (let i = 0; i < this.activities.length; i++) {
-              this.activities[i].observacion = '';
-              if(this.activities[i].tipo_de_respuesta__c == 'true'){
-                this.activities[i].valor = false;
-              }else{
-                this.activities[i].valor = undefined;
-              }
-            }
-        })
+    if (idTipoRutina && turno) {
+
+
+      this.dbService.getPreguntasTipoRutinaOffline(idTipoRutina, turno).then(data => {
+        // console.log("TIPOS RUTINA: " + JSON.stringify(data))
+        this.activities = data;
+        for (let i = 0; i < this.activities.length; i++) {
+          this.activities[i].observacion = '';
+          if (this.activities[i].tipo_de_respuesta__c == 'true') {
+            this.activities[i].valor = false;
+          } else {
+            this.activities[i].valor = undefined;
+          }
+        }
+      })
       // this.rutinasProv.getPreguntasTipoRutina(idTipoRutina, turno).subscribe(data =>{
 
       //   // Agrego el campo observación vacío, y el tipo seteo el tipo de
@@ -263,33 +308,33 @@ export class NuevaRutinaPage {
 
   //Método que verifica si hay alguna respuesta incompleta
   //para deshabilitar el boton de crear
-  respuestasIncompletas(){
+  respuestasIncompletas() {
     for (let i = 0; i < this.activities.length; i++) {
-        if(this.activities[i].valor == undefined || (this.activities[i].tipo_de_respuesta__c == 'false' && this.activities[i].valor == ""))
-          return true;
+      if (this.activities[i].valor == undefined || (this.activities[i].tipo_de_respuesta__c == 'false' && this.activities[i].valor == ""))
+        return true;
     }
 
     return false;
   }
 
-  crearRutina(){
+  crearRutina() {
     this.loading = true;
     var listaActividades = [];
     for (let i = 0; i < this.activities.length; i++) {
-        listaActividades.push(
-          {
-            'id_pregunta_rutina__c': this.activities[i].sfid,
-            'valor_si_no__c' : this.activities[i].tipo_de_respuesta__c == 'true' ? this.activities[i].valor : null,
-            'valornumerico__c' :this.activities[i].tipo_de_respuesta__c  == 'false' ? this.activities[i].valor : null,
-            'observaciones__c' : !this.activities[i].observaciones ? '' : this.activities[i].observaciones,
-            'foto1__c': !this.activities[i].foto1__c ? '' : this.activities[i].foto1__c,
-            'foto2__c': !this.activities[i].foto2__c ? '' : this.activities[i].foto2__c
-          });
+      listaActividades.push(
+        {
+          'id_pregunta_rutina__c': this.activities[i].sfid,
+          'valor_si_no__c': this.activities[i].tipo_de_respuesta__c == 'true' ? this.activities[i].valor : null,
+          'valornumerico__c': this.activities[i].tipo_de_respuesta__c == 'false' ? this.activities[i].valor : null,
+          'observaciones__c': !this.activities[i].observaciones ? '' : this.activities[i].observaciones,
+          'foto1__c': !this.activities[i].foto1__c ? '' : this.activities[i].foto1__c,
+          'foto2__c': !this.activities[i].foto2__c ? '' : this.activities[i].foto2__c
+        });
     }
 
     var data = {
-      'observacion__c' : this.observacion,
-      'idtiporutina__c' : this.tipoRutina,
+      'observacion__c': this.observacion,
+      'idtiporutina__c': this.tipoRutina,
       "idplanta__c": this.authservice.AuthToken.planta.sfid,
       'usuarioapp__c': this.authservice.AuthToken.usuario.sfid,
       'rutaimagen__c': '',
@@ -297,66 +342,66 @@ export class NuevaRutinaPage {
       'actividadrutina__c': listaActividades
     }
 
-    if(this.network.type == 'none' || this.network.type == 'unknown'){
+    if (this.network.type == 'none' || this.network.type == 'unknown') {
       // console.log("rutina a crear OFFLINE: " + JSON.stringify(data));
-      
+
       this.dbService.crearRutinaOffline(data).then(response => {
-          // console.log("RESPONSE: " + response);
-          // console.log("RESPONSE: " + JSON.stringify(response));
-          if(response){
-            if(this.images.length > 0){
+        // console.log("RESPONSE: " + response);
+        // console.log("RESPONSE: " + JSON.stringify(response));
+        if (response) {
+          if (this.images.length > 0) {
+            this.moverArchivo(this.images, response);
+          }
+          this.loading = false;
+          this.navCtrl.pop();
+        } else {
+          this.loading = false;
+        }
+
+      }, error => {
+        console.log("ERROR CREANDO");
+
+      })
+    } else {
+      // this.dbService.crearRutinaOffline(data).then(response => {
+      //   console.log("RESPONSE: " + response);
+      //   console.log("RESPONSE: " + JSON.stringify(response));
+      // }, error=>{
+      //   console.log("ERROR CREANDO");
+
+      // })
+      console.log("rutina a crear ONLINE: " + JSON.stringify(data));
+      this.rutinasProv.crearRutina(data).then(response => {
+        if (this.network.type == 'none' || this.network.type == 'unknown') {
+          if (response) {
+            if (this.images.length > 0) {
               this.moverArchivo(this.images, response);
             }
             this.loading = false;
             this.navCtrl.pop();
-          }else{
+          } else {
             this.loading = false;
           }
-                
-        }, error=>{
-          console.log("ERROR CREANDO");
-          
-        })
-    }else{
-        // this.dbService.crearRutinaOffline(data).then(response => {
-        //   console.log("RESPONSE: " + response);
-        //   console.log("RESPONSE: " + JSON.stringify(response));
-        // }, error=>{
-        //   console.log("ERROR CREANDO");
-          
-        // })
-        console.log("rutina a crear ONLINE: " + JSON.stringify(data));        
-        this.rutinasProv.crearRutina(data).then(response=>{
-          if(this.network.type == 'none' || this.network.type == 'unknown'){
-            if(response){
-              if(this.images.length > 0){
-                this.moverArchivo(this.images, response);
-              }
-              this.loading = false;
-              this.navCtrl.pop();
-            }else{
-              this.loading = false;
+        } else {
+          if (response) {
+            if (this.images.length > 0) {
+              this.uploadImages(this.images, response);
             }
-          }else{
-            if(response){
-              if(this.images.length > 0){
-                this.uploadImages(this.images, response);
-              }
-              this.loading = false;
-              this.navCtrl.pop();
-            }else{
-              this.loading = false;
-            }
+            this.loading = false;
+            this.navCtrl.pop();
+          } else {
+            this.loading = false;
           }
-    
-        }, error=>{
-          this.loading = false;
-        });
-      
+        }
+
+      }, error => {
+        this.loading = false;
+      });
+
     }
   }
 
-  uploadImages(images, id){
+  uploadImages(images, id) {
     let options: FileUploadOptions = {
       fileKey: 'azureupload',
       // fileName: fileName,
@@ -364,7 +409,7 @@ export class NuevaRutinaPage {
       mimeType: "image/jpeg",
       // mimeType: 'multipart/form-data',
       // headers: {},
-      params : {'containername': "rutina" + id.toString()}
+      params: { 'containername': "rutina" + id.toString() }
     }
 
     const fileTransfer: FileTransferObject = this.transfer.create();
@@ -376,12 +421,12 @@ export class NuevaRutinaPage {
       // console.log(images[i]);
       options.fileName = images[i].path.substring(images[i].path.lastIndexOf('/') + 1, images[i].path.length);
       fileTransfer.upload(images[i].path, URL_SERVICIOS + '/azurecrearcontenedorsubirimagen', options)
-      .then((data) => {
-        // console.log(data+" Uploaded Successfully");
+        .then((data) => {
+          // console.log(data+" Uploaded Successfully");
 
-      }, (err) => {
-        console.log('Error:' + JSON.stringify(err));
-      });
+        }, (err) => {
+          console.log('Error:' + JSON.stringify(err));
+        });
     }
 
     // console.log("UPLOADING");
--- src/providers/auth-service/auth-service.ts
+++ src/providers/auth-service/auth-service.ts
@@ -175,4 +175,27 @@ export class AuthService {
       alert.present();
     }
 
+  validaUbicacion(lat,lng) {
+    var R = 6371; // Radius of the earth in km
+    var lat1 = lat;
+    var lng1 = lng;
+    var lat2 = this.AuthToken.planta.billinglatitude;
+    var lng2 = this.AuthToken.planta.billinglongitude;
+    var dLat = this.deg2rad(lat2-lat1);  // deg2rad below
+    var dLon = this.deg2rad(lng2-lng1);
+    var a =
+            Math.sin(dLat/2) * Math.sin(dLat/2) +
+            Math.cos(this.deg2rad(lat1)) * Math.cos(this.deg2rad(lat2)) *
+            Math.sin(dLon/2) * Math.sin(dLon/2)
+            ;
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    var distancia = R * c; // Distance in km
+    distancia = distancia * 1000;
+    return distancia > this.AuthToken.planta.radio__c;
+  }
+  //Convierto en radianes
+  deg2rad(deg) {
+    return deg * (Math.PI/180)
+  }
+
 }
