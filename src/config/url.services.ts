9085db432224e7f683836f8984141f3110c19830
Dacev, Jorge Javier <jdacev@grupoassa.com>
2018-08-22 10:56:54 -0300

    URL apuntando a Heroku quality assurance

--- src/config/url.services.ts
+++ src/config/url.services.ts
@@ -1,2 +1,1 @@
-export const URL_SERVICIOS = "https://rotoplasprod.herokuapp.com/api"; /*Produccion*/
-// export const URL_SERVICIOS = "https://rotoplasoperarios.herokuapp.com/api"; /*Desarrollo*/
+export const URL_SERVICIOS = "https://rotoplasqa.herokuapp.com/"; /*quality-assurance*/
